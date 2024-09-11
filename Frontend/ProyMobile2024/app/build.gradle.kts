477bbbec0b26329635ceb6cb2c3cc3b96152fa9d
ValeAngeletti <valeangelett11@gmail.com>
2024-09-11 19:04:46 -0300

    Fragment Perfil

--- Frontend/ProyMobile2024/app/build.gradle.kts
+++ Frontend/ProyMobile2024/app/build.gradle.kts
@@ -37,6 +37,7 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
+    implementation("androidx.fragment:fragment:1.8.3")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
