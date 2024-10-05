f73bece29cce99a2b03f8d7f4f1098c338556067
FlorenciaCastelucci <florcastelucci97@gmail.com>
2024-10-04 21:16:20 -0300

    configuracion AUTH0 (dependencias y manifest)

--- Frontend/ProyMobile2024/app/build.gradle.kts
+++ Frontend/ProyMobile2024/app/build.gradle.kts
@@ -56,6 +56,8 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
     implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+    implementation("com.auth0.android:auth0:2.11.0")
+    implementation("com.auth0.android:lock:2.23.0")
 
 
     //implementation(libs.coordinatorlayout)
