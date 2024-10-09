9c64ac5e51c8dbe45cfa75d19c45b0443e535fee
TauTech99 <taoufik.saidi@estudiantes.unahur.edu.ar>
2024-10-09 14:09:20 -0300

    Added Postman collection and Newman report for API automated tests (CRUD)

--- Frontend/ProyMobile2024/app/build.gradle.kts
+++ Frontend/ProyMobile2024/app/build.gradle.kts
@@ -36,7 +36,7 @@ android {
 }
 
 dependencies {
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
