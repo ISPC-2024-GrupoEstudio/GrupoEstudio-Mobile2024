a1b5b153c559b127ada72a8aac0e5f70667b746b
Milena Gimenez <milenanicolegimenez@gmail.com>
2024-10-09 12:41:47 -0300

    agregando manifestPlaceHolders a build.gradle para correcta ejecucion del proyecto en emulador

--- Frontend/ProyMobile2024/app/build.gradle.kts
+++ Frontend/ProyMobile2024/app/build.gradle.kts
@@ -1,5 +1,17 @@
+import org.apache.tools.ant.util.JavaEnvUtils.VERSION_1_8
+
 plugins {
     alias(libs.plugins.android.application)
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.7.0")
+    }
 }
 
 android {
@@ -14,6 +26,12 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Añadir placeholders del manifiesto
+        manifestPlaceholders.putAll(mapOf(
+            "auth0Domain" to "dev-beq8aquv5nb4mfcn.us.auth0.com",
+            "auth0Scheme" to "https"
+        ))
     }
 
     buildTypes {
@@ -33,15 +51,16 @@ android {
         viewBinding = true
         dataBinding = true
     }
+    buildToolsVersion = "34.0.0"
 }
 
 dependencies {
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("androidx.fragment:fragment:1.8.3")
+    implementation("androidx.fragment:fragment:1.8.4")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
     implementation("androidx.activity:activity:1.9.2")
@@ -49,14 +68,14 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("com.github.bumptech.glide:glide:4.12.0")
-    implementation("com.google.code.gson:gson:2.10.1")
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.11.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
     implementation("com.auth0.android:auth0:2.11.0")
-    implementation("com.auth0.android:lock:2.23.0")
+    implementation("com.auth0.android:lock:3.2.2")
 
     //implementation(libs.coordinatorlayout)
 }
