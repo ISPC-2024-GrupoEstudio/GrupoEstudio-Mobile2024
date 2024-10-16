a42ba9629edb8e5e1443d498dc2a84c6b1511b15
ValeAngeletti <valeangelett11@gmail.com>
2024-10-16 16:56:38 -0300

    correccion estilos landing

--- Frontend/ProyMobile2024/app/src/main/java/com/example/proy_mobile2024/services/ApiService.java
+++ Frontend/ProyMobile2024/app/src/main/java/com/example/proy_mobile2024/services/ApiService.java
@@ -1,11 +1,16 @@
 package com.example.proy_mobile2024.services;
+import com.example.proy_mobile2024.model.Perfil;
+
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
 
 
 public interface ApiService {
     // 1. Solicitud de inicio de sesión (POST)
-    @POST("api/login/")
-    Call<Void> loginUser(@Body LoginData loginData);
+//    @POST("api/login/")
+//    Call<Void> loginUser(@Body LoginData loginData);
+      @GET("api/perfil/")
+      Call<Perfil> getPerfil();
 }
