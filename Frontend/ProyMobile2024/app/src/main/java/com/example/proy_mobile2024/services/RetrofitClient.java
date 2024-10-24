efef3b9dc8210f47737fb8c730d2241457635337
FlorenciaCastelucci <florcastelucci97@gmail.com>
2024-10-23 22:16:43 -0300

    .

--- Frontend/ProyMobile2024/app/src/main/java/com/example/proy_mobile2024/services/RetrofitClient.java
+++ Frontend/ProyMobile2024/app/src/main/java/com/example/proy_mobile2024/services/RetrofitClient.java
@@ -11,7 +11,7 @@ public class RetrofitClient {
     private Retrofit retrofit = null;
     private static RetrofitClient instance = null;
     private ApiService apiService;
-    private static final String BASE_URL = "https://6cf2-181-92-28-8.ngrok-free.app/api/";
+    private static final String BASE_URL = "https://e890-190-136-244-190.ngrok-free.app/api/";
 
 
     private RetrofitClient(Context context) {
--- backend/PetBoutique/PetBoutique/settings.py
+++ backend/PetBoutique/PetBoutique/settings.py
@@ -32,12 +32,12 @@ ALLOWED_HOSTS = [
     'localhost',
     '127.0.0.1',
     '10.0.2.2',
-    'ae5a-190-136-244-190.ngrok-free.app',
-    '6cf2-181-92-28-8.ngrok-free.app'
+    '6cf2-181-92-28-8.ngrok-free.app',
+    'e890-190-136-244-190.ngrok-free.app'
     ]
 
 CSRF_TRUSTED_ORIGINS = [
-    'https://ae5a-190-136-244-190.ngrok-free.app',
+    'https://e890-190-136-244-190.ngrok-free.app',
     'https://6cf2-181-92-28-8.ngrok-free.app'
 ]
 
@@ -87,7 +87,7 @@ SIMPLE_JWT = {
 
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:4200',
-    "https://ae5a-190-136-244-190.ngrok-free.app",
+    'https://e890-190-136-244-190.ngrok-free.app',
     "https://6cf2-181-92-28-8.ngrok-free.app"
 ]
 
@@ -123,7 +123,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'pet_boutique',
         'USER': 'root',
-        'PASSWORD': 'contra.bd.24',
+        'PASSWORD': 'mibasededatos',
         'HOST': 'localhost',
         'PORT': '3306',
         'OPTIONS': {
